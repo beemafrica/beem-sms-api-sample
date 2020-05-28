78727d00f4af9528e5ff89fe74876a200b76dbeb
stephano <stephano@bongolive.co.tz>
2020-05-28 10:45:17 +0300

    samples 2020 may27

--- send/node-js/send.js
+++ send/node-js/send.js
@@ -1,19 +1,34 @@
-var request = require("request");
+const https = require("https");
 
-var options = { method: 'GET',
-    url: 'http://www.bongolive.co.tz/api/sendSMS.php?',
-    qs: 
-        { sendername: 'Enter your sendername',
-            username: 'Enter username',
-            password: 'Enter your password',
-            apikey: 'Enter your apikey',
-            destnum: 'Enter your destnation number',
-            message: 'Enter your message',
-            senddate: '',
-            format: 'json' },
-        headers: { 'cache-control': 'no-cache' } };
+function send_sms() {
+  axios.post(
+    "https://dev-sms.bongolive.africa/api/v1/send",
+    {
+      source_addr: "INFO",
+      schedule_time: "",
+      message: "Hello World",
+      recipients: [
+        {
+          recipient_id: 1,
+          dest_addr: "255700000001",
+        },
+        {
+          recipient_id: 2,
+          dest_addr: "255700000002",
+        },
+      ],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic NWYwOTU4ZDM1ZDgwM2ZlYzpNamhsTUdGbFkyWXpObUk1TVRZd01qZzRZamhtWW1GbU5HSTJOakZsTnprMU16WXlaRFJsTTJOa09HWXdZamd3TlRZeU1HUm1aRFUwTldGa05qSTBOZz09",
+      },
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    }
+  ).then(response=>console.log(response)).catch(error=>console.error(error.response.data))
+}
 
-request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-        console.log(body);
-            });
+send_sms();
